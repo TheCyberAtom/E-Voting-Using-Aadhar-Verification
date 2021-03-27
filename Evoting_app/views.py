e77c74510fac86002895baa66e71c4990da20bb0
Rahul Kumar Mishra <50443941+TheCyberAtom@users.noreply.github.com>
2021-03-27 15:13:32 +0530

    updates

--- Evoting_app/views.py
+++ Evoting_app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from .models import Candidate,Voter
 
 
 def index(request):
     return render(request,'index.html')
 
 def register(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        mobile = request.POST.get('mobile')
+        state = request.POST.get('state')
+        date = request.POST.get('date')
+        sex = request.POST.get('sex')
+        print(name, mobile, state, date, sex)
+
+
     return render(request,'register.html')
